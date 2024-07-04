04cba9cc1b00ae65e245d1b4d83f301c5b06308a
zhangxiang <zhangxiang@uphicoo.com>
2024-07-04 11:09:26 +0800

    init

--- reference/src/main.rs
+++ reference/src/main.rs
@@ -1,9 +1,26 @@
 fn main() {
     let str = String::from("hello");
     let len = calculate_length(&str);
-    println!("{}", len)
+    println!("{}, {}", str, len);
+
+    let mut str1 = String::from("hello");
+    push_str_test(&mut str1);
+    print!("{}", str1);
+    no_use();
 }
 
 fn calculate_length(s: &String) -> usize {
     s.len()
+}
+
+fn push_str_test(s: &mut String) {
+    s.push_str(", world")
+}
+
+fn no_use() {
+    let mut s = String::from("hello");
+
+    let _r1 = &s;
+    let _r2 = &s;
+    let _r3 = &mut s;
 }
